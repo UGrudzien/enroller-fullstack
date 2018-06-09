bb4ae5e11d3a332de6afcd558838beea85e4b60d
student27 <student27@pcoip.ki.agh.edu.pl>
2018-06-09 15:42:57 +0200

    final from classes

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -29,7 +29,7 @@ public class ParticipantService {
     }
 
     public Participant add(Participant participant) {
-        String hashedPassword = passwordEncoder.encode(participant.setPassword();
+        String hashedPassword = passwordEncoder.encode(participant.getPassword());
         participant.setPassword(hashedPassword);
         Transaction transaction = connector.getSession().beginTransaction();
 
